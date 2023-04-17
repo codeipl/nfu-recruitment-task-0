1b432de1846f6d0dd3ea065038bfc82f7189e256
d <roseannee.kim@gmail.com>
2023-04-17 22:52:12 +0200

    feat(form): update form interaction and add validation with react-hook-form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
 import axios from 'axios'
 import mainImg from '../assets/main.jpg'
 
+type FormData = {
+    username: string;
+    password: string;
+};
+
 export default function Login() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const [answer, setAnswer] = useState('')
     const [image, setImage] = useState('')
@@ -27,8 +32,10 @@ export default function Login() {
         fetchData()
     })
 
-    const handleSubmit = () => {
-        navigate('/result', { state: { answer: answer, image: image } })
+    const onSubmit = () => {
+        let newAns = 'yes'
+        let newImg = 'https://yesno.wtf/assets/yes/15-3d723ea13af91839a671d4791fc53dcc.gif'
+        navigate(`/result/${newAns}/${encodeURIComponent(newImg)}`)
     }
 
     return (
@@ -39,20 +46,33 @@ export default function Login() {
             </div>
             <div className='flex items-center justify-center bg-[#2e4d41]'>
                 <form 
-                    className='rounded-lg shadow-2xl bg-[#738b73] p-8 mx-4'>
+                    className='rounded-lg shadow-2xl bg-[#738b73] p-8 mx-4'
+                    onSubmit={ handleSubmit(onSubmit) }>
 
                     <h1 className='text-4xl font-bold uppercase text-white text-center'>Welcome</h1>
                     <div>
                         <label className='block text-[#1d3329] text-sm font-bold py-2 mt-4'>Username</label>
                         <input
-                            className={`w-full rounded border-2 text-[#1d3329] py-1 px-2 outline-none transition-all duration-200 ease-linear hover:border-[#2e4d41] focus:bg-[#2e4d41] focus:border-[#2e4d41] focus:text-white`}
-                            type='text' />
+                            className={`w-full rounded border-2 text-[#1d3329] py-1 px-2 outline-none transition-all duration-200 ease-linear hover:border-[#2e4d41] focus:bg-[#2e4d41] focus:border-[#2e4d41] focus:text-white
+                            ${errors.username ? "border-red-400 hover:border-red-500" : "border-transparent"}`}
+                            type='text' {...register("username", { required: true })}/>
+                        <span 
+                            className='text-white text-sm'
+                            style={{ visibility: errors.username ? "visible" : "hidden" }}>
+                                This field is required!
+                        </span>
                     </div>
                     <div>
                         <label className='block text-[#1d3329] text-sm font-bold py-2'>Password</label>
                         <input 
-                            className={`w-full rounded border-2 text-[#1d3329] py-1 px-2 outline-none transition-all duration-200 ease-linear hover:border-[#2e4d41] focus:bg-[#2e4d41] focus:border-[#2e4d41] focus:text-white`}
-                            type='password' />
+                            className={`w-full rounded border-2 text-[#1d3329] py-1 px-2 outline-none transition-all duration-200 ease-linear hover:border-[#2e4d41] focus:bg-[#2e4d41] focus:border-[#2e4d41] focus:text-white
+                            ${errors.password ? "border-red-400 hover:border-red-500" : "border-transparent"}`}
+                            type='password' {...register("password", { required: true })}/>
+                        <span 
+                            className='text-white text-sm'
+                            style={{ visibility: errors.password ? "visible" : "hidden" }}>
+                                This field is required!
+                        </span>
                     </div>
                     <button 
                         className='w-full h-9 border-2 border-[#2e4d41] bg-[#2e4d41] text-white font-semibold shadow-lg rounded-lg mt-4 transition-all duration-300 ease-linear hover:bg-white hover:text-black'
