1614ae9926f6436717373abd1eb16ef29e5673d9
SebastianK2412 <sebastian.kula.24.12@gmail.com>
2023-04-27 10:21:40 +0200

    refactor: refactor to functional component

--- src/components/MyFooter.tsx
+++ src/components/MyFooter.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-export default class MyFooter extends React.Component{
-    render(){
-        return (
-            <div className="footer">
-                Made with passion by Sebastian
-            </div>
-        );
-    }
+export default function MyFooter(){
+    return (
+        <div className="footer">
+            Made with passion by Sebastian
+        </div>
+    );
 }
--- src/components/MyHeader.tsx
+++ src/components/MyHeader.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-export default class MyHeader extends React.Component{
-    render(){
-        return (
-            <div className="header">
-                <div className="website-title"><h1>Sebastian NFU Requirement Task</h1><br />
-                <h2>Hope for the best</h2></div>
-                <img src="src/images/HeaderGraphic.png"/>
-            </div>
-        );
-    }
+export default function MyHeader() {
+    return (
+        <div className="header">
+        <div className="website-title"><h1>Sebastian NFU Requirement Task</h1><br />
+        <h2>Hope for the best</h2></div>
+        <img src="src/images/HeaderGraphic.png"/>
+        </div>
+    );
 }
